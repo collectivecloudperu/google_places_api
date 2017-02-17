1bb7557f5984f34c255ccda7e6776384fadde32f
Collective Cloud Peru <collectivecloudperu@users.noreply.github.com>
2017-02-17 03:52:30 -0500

    Cambios
    Cambios API 3

--- escuelas/index.php
+++ escuelas/index.php
@@ -7,7 +7,7 @@
 	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">    
 	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-		<script src="https://maps.googleapis.com/maps/api/js?libraries=places" type="text/javascript"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?key=TUAPIKEYACA&libraries=places&callback=initMap" async defer></script>
 
 		<style type="text/css">
 			#map {
@@ -42,99 +42,88 @@
 
 
 		<script type="text/javascript">
-			var map;
-			var infowindow = new google.maps.InfoWindow();
-			var pos;
+	      function initMap() {
 
-			google.maps.event.addDomListener(window, "resize", function() {
-			    var center = map.getCenter();
-			    google.maps.event.trigger(map, "resize");
-			    map.setCenter(center); 
-			});
+	        if (navigator.geolocation) { //Geolocalizar
+	          navigator.geolocation.getCurrentPosition(function(position) {
 
-			function initMap() {
+	            pos = {
+	              lat: position.coords.latitude/*-12.0872261*/,
+	              lng: position.coords.longitude/*-77.0029128*/
+	            }
+	            map = new google.maps.Map(document.getElementById('map'), {
+	              center: myLocation,
+	              zoom: 17
+	            });
 
-			  if (navigator.geolocation) { //Geolocalizar
-			    navigator.geolocation.getCurrentPosition(function(position) {
+	            infoWindow = new google.maps.InfoWindow({
+	              map: map
+	            });
 
-			      pos = {
-			        lat: position.coords.latitude/*-12.0872261*/,
-			        lng: position.coords.longitude/*-77.0029128*/
-			      }
-			      map = new google.maps.Map(document.getElementById('map'), {
-			        center: myLocation,
-			        zoom: 17
-			      });
+	            infoWindow.setPosition(pos);
+	            infoWindow.setContent('Mi ubicación.');
+	            map.setCenter(pos);
+	            var myLocation = pos;
 
-			      infoWindow = new google.maps.InfoWindow({
-			        map: map
-			      });
+	            var service = new google.maps.places.PlacesService(map);
+	            service.nearbySearch({
+	              location: myLocation,
+	              radius: 500, /* Que detecte los lugares en un Radio de 500 metros a la redonda */
+	              types: ['school']
+	            }, callback);
+	          })
+	        };
+	      }
 
-			      infoWindow.setPosition(pos);
-			      infoWindow.setContent('Mi ubicación.');
-			      map.setCenter(pos);
-			      var myLocation = pos;
+	      function callback(results, status) {
+	        if (status === google.maps.places.PlacesServiceStatus.OK) {
+	          for (var i = 0; i < results.length; i++) {
+	            createMarker(results[i]);
+	          }
+	        }
+	      }
 
-			      var service = new google.maps.places.PlacesService(map);
-			      service.nearbySearch({
-			        location: myLocation,
-			        radius: 500, /* Que detecte los lugares en un Radio de 500 metros a la redonda */
-			        types: ['school']
-			      }, callback);
-			    })
-			  };
-			}
+	      function createMarker(place) {
+	        var placeLoc = place.geometry.location;
+	          if (place.icon) {
+	            var image = new google.maps.MarkerImage(
+	                  place.icon, new google.maps.Size(71, 71),
+	                  new google.maps.Point(0, 0), new google.maps.Point(17, 34),
+	                  new google.maps.Size(25, 25));
+	          } else var image = null;
 
-			function callback(results, status) {
-			  if (status === google.maps.places.PlacesServiceStatus.OK) {
-			    for (var i = 0; i < results.length; i++) {
-			      createMarker(results[i]);
-			    }
-			  }
-			}
+	          var marker = new google.maps.Marker({
+	            map: map,
+	            icon: image,
+	            position: place.geometry.location
+	          });
 
-			function createMarker(place) {
-		    var placeLoc = place.geometry.location;
-			    if (place.icon) {
-			      var image = new google.maps.MarkerImage(
-			            place.icon, new google.maps.Size(71, 71),
-			            new google.maps.Point(0, 0), new google.maps.Point(17, 34),
-			            new google.maps.Size(25, 25));
-			    } else var image = null;
+	          service = new google.maps.places.PlacesService(map);
 
-			    var marker = new google.maps.Marker({
-			      map: map,
-			      icon: image,
-			      position: place.geometry.location
-			    });
+	          var request =  {
+	            reference: place.reference
+	          };
 
-			    service = new google.maps.places.PlacesService(map);
+	          google.maps.event.addListener(marker,'click',function(){
+	              service.getDetails(request, function(place, status) {
+	                if (status == google.maps.places.PlacesServiceStatus.OK) {
+	                  var contentStr = '<h5><strong>'+place.name+'</strong></h5><p>'+place.formatted_address;
+	                  if (!!place.formatted_phone_number) contentStr += '<br>'+place.formatted_phone_number;
+	                  if (!!place.website) contentStr += '<br><a target="_blank" href="'+place.website+'">'+place.website+'</a>';
+	                  /* contentStr += '<br>'+place.types+'</p>'; */
+	                  contentStr += '<br>ID: '+place.place_id+'</p>';
+	                  infowindow.setContent(contentStr);
+	                  infowindow.open(map,marker);
+	                } else { 
+	                  var contentStr = "<h5>No Hubo Resultados, status="+status+"</h5>";
+	                  infowindow.setContent(contentStr);
+	                  infowindow.open(map,marker);
+	                }
+	              });
+	          });
+	      }
 
-			    var request =  {
-			      reference: place.reference
-			    };
-
-			    google.maps.event.addListener(marker,'click',function(){
-			        service.getDetails(request, function(place, status) {
-			          if (status == google.maps.places.PlacesServiceStatus.OK) {
-			            var contentStr = '<h5><strong>'+place.name+'</strong></h5><p>'+place.formatted_address;
-			            if (!!place.formatted_phone_number) contentStr += '<br>'+place.formatted_phone_number;
-			            if (!!place.website) contentStr += '<br><a target="_blank" href="'+place.website+'">'+place.website+'</a>';
-			            /* contentStr += '<br>'+place.types+'</p>'; */
-			            contentStr += '<br>ID: '+place.place_id+'</p>';
-			            infowindow.setContent(contentStr);
-			            infowindow.open(map,marker);
-			          } else { 
-			            var contentStr = "<h5>No Hubo Resultados, status="+status+"</h5>";
-			            infowindow.setContent(contentStr);
-			            infowindow.open(map,marker);
-			          }
-			        });
-			    });
-			}
-
-			initMap();
-		</script>		
+    </script>			
 		    
 		
 
